8ae5bd26ce9c9a3a6e4f3d7b308242ef8060e61e
ChivasQ <a6plus18@gmail.com>
2026-05-20 12:35:16 +0300

    little styles

--- nbt-editor/src/main.ts
+++ nbt-editor/src/main.ts
@@ -13,52 +13,20 @@ let list_buffer = new DocumentFragment();
 for (const element of blocks) {
     let card = document.createElement('div');
     card.className = "block-card";
-    card.style = "width:32px; height:32px;";
-
-    let image = document.createElement('img');
-    image.src = `/api/assets/preview?name=${element.name}`;
-    image.style = "width:32px; height:32px;";
-    image.loading = "lazy";
-    card.appendChild(image);
+    card.dataset.blockName = element.name;
+    card.innerHTML = `<img src="/api/assets/preview?name=${element.name}" width="48" height="48" loading="lazy">`;
     list_buffer.appendChild(card);
 }
-
 block_list?.appendChild(list_buffer);
 
+block_list?.addEventListener('click', (e) => {
+    const target = (e.target as HTMLElement).closest('.block-card');
+    if (target) {
+        const name = (target as HTMLElement).dataset.blockName;
+        console.log("Choosed block:", name);
 
-
-
-
-
-
-
-// const modelData = await MinecraftAPI.getBlockModel("minecraft:block/anvil");
-// const modelBlockStates = await MinecraftAPI.getBlockStates("minecraft:anvil");
-// console.log(modelBlockStates)
-// // console.log(modelData);
-// const mesh = editor.getMeshFromJson(modelData);
-
-// if (mesh) {
-//     editor.addBlock(0, 0, 0, mesh);
-// }
-
-// const modelData1 = await MinecraftAPI.getBlockModel("minecraft:block/purpur_pillar");
-// const modelBlockStates1 = await MinecraftAPI.getBlockStates("minecraft:purpur_pillar");
-// console.log(modelBlockStates1)
-// console.log(modelData1);
-// const mesh1 = editor.getMeshFromJson(modelData1);
-
-// if (mesh1) {
-//     editor.addBlock(0, 0, 16, mesh1);
-// }
-
-
-// const modelData2 = await MinecraftAPI.getBlockModel("minecraft:block/cauldron");
-// const mesh2 = editor.getMeshFromJson(modelData2);
-
-// if (mesh2) {
-//     editor.addBlock(0, 0, 32, mesh2);
-// }
+    }
+});
 
 
 document.getElementById('file-input')?.addEventListener('change', async (event) => {
